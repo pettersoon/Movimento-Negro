ed994d04cbac8c193f694eff84b58c2eda26e702
pettersoon <petterson.viturino@bantec.com.br>
2021-05-24 19:15:43 -0300

    Cadastro e login configurados

--- API-projeto-site/routes/usuarios.js
+++ API-projeto-site/routes/usuarios.js
@@ -9,10 +9,10 @@ let sessoes = [];
 router.post('/autenticar', function(req, res, next) {
 	console.log('Recuperando usuário por login e senha');
 
-	var login = req.body.login; // depois de .body, use o nome (name) do campo em seu formulário de login
+	var login = req.body.email; // depois de .body, use o nome (name) do campo em seu formulário de login
 	var senha = req.body.senha; // depois de .body, use o nome (name) do campo em seu formulário de login	
 	
-	let instrucaoSql = `select * from usuario where login='${login}' and senha='${senha}'`;
+	let instrucaoSql = `select * from usuario where email='${login}' and senha='${senha}'`;
 	console.log(instrucaoSql);
 
 	sequelize.query(instrucaoSql, {
@@ -57,27 +57,35 @@ router.post('/cadastrar', function(req, res, next) {
 
 
 /* Verificação de usuário */
-router.get('/sessao/:login', function(req, res, next) {
-	let login = req.params.login;
-	console.log(`Verificando se o usuário ${login} tem sessão`);
+// router.get('/sessao/login', function(req, res, next) {
+// 	console.log(`Verificando se o usuário ${login} tem sessão`);
 	
-	let tem_sessao = false;
-	for (let u=0; u<sessoes.length; u++) {
-		if (sessoes[u] == login) {
-			tem_sessao = true;
-			break;
-		}
-	}
+// 	var login = req.body.email; // depois de .body, use o nome (name) do campo em seu formulário de login
+//     var senha = req.body.senha; // depois de .body, use o nome (name) do campo em seu formulário de login
 
-	if (tem_sessao) {
-		let mensagem = `Usuário ${login} possui sessão ativa!`;
-		console.log(mensagem);
-		res.send(mensagem);
-	} else {
-		res.sendStatus(403);
-	}
-	
-});
+//     let instrucaoSql = `select * from usuario where login='${login}' and senha='${senha}'`;
+//     console.log(instrucaoSql);
+
+//     sequelize.query(instrucaoSql, {
+//         model: Usuario
+//     }).then(resultado => {
+//         console.log(`Encontrados: ${resultado.length}`);
+
+//         if (resultado.length == 1) {
+//             sessoes.push(resultado[0].dataValues.login);
+//             console.log('sessoes: ',sessoes);
+//             res.json(resultado[0]);
+//         } else if (resultado.length == 0) {
+//             res.status(403).send('Login e/ou senha inválido(s)');
+//         } else {
+//             res.status(403).send('Mais de um usuário com o mesmo login e senha!');
+//         }
+
+//     }).catch(erro => {
+//         console.error(erro);
+//         res.status(500).send(erro.message);
+//       });
+// });
 
 
 /* Logoff de usuário */
